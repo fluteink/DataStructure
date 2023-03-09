4c825be7efe87486fbcec5e72809b29e43c2e5dc
fluteink <884733966@qq.com>
2023-03-09 20:23:11 +0800

    插入

--- 408/SinglyLinkList.cpp
+++ 408/SinglyLinkList.cpp
@@ -7,7 +7,50 @@
 #include <string.h>
 #include <cmath>
 
+// 带头结点的单链表
+typedef int ElemType;
+typedef struct LNode
+{
+    ElemType data;
+    struct LNode *next;
+} LNode, *LinkList;
+bool InitList(LinkList &L)
+{
+    L = (LNode *)malloc(sizeof(LNode));
+    if (L == NULL) // 内存不足，分配失败
+    {
+        return false;
+    }
+    L->next = NULL; // 头结点之后暂时还没有节点
+    return true;
+}
+bool ListInsert(LinkList &L, int i, ElemType e)
+{
+    if (i < 1)
+    {
+        return false;
+    }
+    LNode *p;
+    int j = 0;
+    p = L;
+    while (p != NULL && j < i - 1)
+    {
+        p = p->next;
+        j++;
+    }
+    if (p == NULL)
+    {
+        return false;
+    }
+    LNode *s = (LNode *)malloc(sizeof(LNode));
+    s->data = e;
+    s->next = p->next;
+    p->next = s;
+    return true;
+}
 int main()
 {
+    LinkList L;
+    InitList(L);
     return 0;
 }
