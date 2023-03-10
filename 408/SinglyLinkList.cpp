5d3e7a3bec2371bf5b84b190f5523b192d67c6e5
fluteink <884733966@qq.com>
2023-03-10 17:06:09 +0800

    后插定义

--- 408/SinglyLinkList.cpp
+++ 408/SinglyLinkList.cpp
@@ -14,6 +14,42 @@ typedef struct LNode
     ElemType data;
     struct LNode *next;
 } LNode, *LinkList;
+LNode *GetElem(LinkList &L, int i) // 找到第i个节点并返回
+{
+    if (i < 0)
+    {
+        return NULL;
+    }
+    LNode *p;
+    p = L;
+    int j = 0;
+    while (p != NULL && j < i)
+    {
+        p = p->next;
+        j++;
+    }
+    return p;
+}
+LNode *LocateElem(LinkList &L, ElemType i) // 按值查找
+{
+    LNode *p = L->next;
+    while (p != NULL && p->data != i)
+    {
+        p = p->next;
+    }
+    return p;
+}
+int Length(LinkList &L) // 求表长
+{
+    LNode *p = L;
+    int i = 0;
+    while (p->next != NULL)
+    {
+        p = p->next;
+        i++;
+    }
+    return i;
+}
 void printList(LinkList &L)
 {
     LNode *p = L->next;
@@ -52,15 +88,7 @@ bool ListInsert(LinkList &L, int i, ElemType e) // 在第i个位置插入元素e
     {
         return false;
     }
-    LNode *p;
-    int j = 0;
-    p = L;
-    while (p != NULL && j < i - 1)
-    {
-        p = p->next;
-        j++;
-    }
-
+    LNode *p = GetElem(L, i - 1);
     return InsertNextNode(p, e);
 }
 bool InsertPriorNode(LNode *p, ElemType e) // 节点前插
@@ -88,14 +116,7 @@ bool ListDelete(LinkList &L, int i, ElemType &e)
     {
         return false;
     }
-    LNode *p;
-    int j = 0;
-    p = L;
-    while (p != NULL && j < i - 1)
-    {
-        p = p->next;
-        j++;
-    }
+    LNode *p = GetElem(L, i - 1);
     if (p == NULL || p->next == NULL)
     {
         /* code */
@@ -121,21 +142,22 @@ bool DeleteNode(LNode *p) // 只能用来删除不是最后一个节点的节点
     free(q);
     return true;
 }
-LNode *GetElem(LinkList &L, int i)
+LinkList List_TailInsert(LinkList &L) // 后插定义
 {
-    if (i < 0)
+    int x;
+    L = (LinkList)malloc(sizeof(LNode));
+    LNode *s, *r = L;
+    scanf("%d", &x);
+    while (x != 9999)
     {
-        return NULL;
+        s = (LNode *)malloc(sizeof(LNode));
+        s->data = x;
+        r->next = s;
+        r = s;
+        scanf("%d", &x);
     }
-    LNode *p;
-    p = L;
-    int j = 0;
-    while (p != NULL && j < i)
-    {
-        p = p->next;
-        j++;
-    }
-    return p;
+    r->next = NULL;
+    return L;
 }
 int main()
 {
@@ -145,11 +167,12 @@ int main()
     LinkList L;
     InitList(L);
     ElemType e;
-    for (size_t i = 1; i <= 4; i++)
-    {
-        ListInsert(L, i, rand() % 100);
-    }
+    // for (size_t i = 1; i <= 0; i++)
+    // {
+    //     ListInsert(L, i, rand());
+    // }
+    List_TailInsert(L);
     printList(L);
-    printf("%d\n", GetElem(L, 3)->data);
+    printf("%d\n", Length(L));
     return 0;
 }
