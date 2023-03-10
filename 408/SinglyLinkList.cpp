ce595f5f9ef29944f2dbf2768cd42f13aebb306b
fluteink <884733966@qq.com>
2023-03-10 16:18:20 +0800

    get

--- 408/SinglyLinkList.cpp
+++ 408/SinglyLinkList.cpp
@@ -22,6 +22,7 @@ void printList(LinkList &L)
         printf("%d\n", p->data);
         p = p->next;
     }
+    printf("------------------------------\n");
 }
 bool InitList(LinkList &L)
 {
@@ -33,7 +34,19 @@ bool InitList(LinkList &L)
     L->next = NULL; // 头结点之后暂时还没有节点
     return true;
 }
-bool ListInsert(LinkList &L, int i, ElemType e)
+bool InsertNextNode(LNode *p, ElemType e) // 在p后面插入一个节点
+{
+    if (p == NULL)
+    {
+        return false;
+    }
+    LNode *s = (LNode *)malloc(sizeof(LNode));
+    s->data = e;
+    s->next = p->next;
+    p->next = s;
+    return true;
+}
+bool ListInsert(LinkList &L, int i, ElemType e) // 在第i个位置插入元素e
 {
     if (i < 1)
     {
@@ -47,24 +60,96 @@ bool ListInsert(LinkList &L, int i, ElemType e)
         p = p->next;
         j++;
     }
+
+    return InsertNextNode(p, e);
+}
+bool InsertPriorNode(LNode *p, ElemType e) // 节点前插
+{
     if (p == NULL)
     {
         return false;
     }
     LNode *s = (LNode *)malloc(sizeof(LNode));
-    s->data = e;
+    if (s == NULL)
+    {
+        /* code */
+        return false;
+    }
+
     s->next = p->next;
     p->next = s;
+    s->data = p->data;
+    p->data = e;
     return true;
+}
+bool ListDelete(LinkList &L, int i, ElemType &e)
+{
+    if (i < 1)
+    {
+        return false;
+    }
+    LNode *p;
+    int j = 0;
+    p = L;
+    while (p != NULL && j < i - 1)
+    {
+        p = p->next;
+        j++;
+    }
+    if (p == NULL || p->next == NULL)
+    {
+        /* code */
+        return false;
+    }
+    LNode *q = p->next;
+    p->next = q->next;
+    e = q->data;
+    free(q);
+    return true;
+}
+bool DeleteNode(LNode *p) // 只能用来删除不是最后一个节点的节点
+
+{
+    if (p == NULL)
+    {
+        return false;
+    }
+    LNode *q = p->next;
+
+    p->data = q->data;
+    p->next = q->next;
+    free(q);
+    return true;
+}
+LNode *GetElem(LinkList &L, int i)
+{
+    if (i < 0)
+    {
+        return NULL;
+    }
+    LNode *p;
+    p = L;
+    int j = 0;
+    while (p != NULL && j < i)
+    {
+        p = p->next;
+        j++;
+    }
+    return p;
 }
 int main()
 {
+
+    srand(time(NULL)); // 设置随机数种子为当前时间
+
     LinkList L;
     InitList(L);
-    for (size_t i = 1; i <= 5; i++)
+    ElemType e;
+    for (size_t i = 1; i <= 4; i++)
     {
-        ListInsert(L, i, i);
+        ListInsert(L, i, rand() % 100);
     }
     printList(L);
+    printf("%d\n", GetElem(L, 3)->data);
     return 0;
 }
