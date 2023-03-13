75b31183c0a42233f66347e299ca03a3d00e336c
fluteink <884733966@qq.com>
2023-03-13 20:09:27 +0800

    55

--- 408/SinglyLinkListWithNoHead.cpp
+++ 408/SinglyLinkListWithNoHead.cpp
@@ -1,328 +1,202 @@
-// 单链表不带头结点
+// 不带头结点的单链表操作
 #include <stdio.h>
-#include <stdlib.h>
+#include <malloc.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <Windows.h>
+typedef int DataType;
+typedef int DataType;
 
-typedef struct LNode
+typedef struct Node
 {
-    int data;
-    struct LNode *next;
-} LNode, *LinkList;
+    DataType Data;
+    struct Node *next;
+} SLNode, *LinkList;
 
-LinkList initList(LinkList list)
+// 初始化
+void ListInitiate(SLNode **head)
 {
-    list = NULL;
-    return list;
+    *head = NULL;
 }
 
-// 插数据（第一个位置）（指定位置）
-LinkList insertFirst(LinkList list, int index, int data)
+// 求当前数据元素个数
+int ListLength(SLNode *head)
 {
-    if (index == 1)
+    SLNode *p;
+    int size = 0;
+    p = head;
+    while (p != NULL)
     {
-        LNode *newNode = (LNode *)malloc(sizeof(LNode));
-        if (newNode == NULL)
+        p = p->next;
+        size++;
+    }
+    return size;
+}
+
+// 插入新结点
+int ListInsert(SLNode **head, int i, DataType x)
+{
+    SLNode *p, *q, *m;
+    int j;
+    if (i == 0) // 若在第0个位置插入
+    {
+        m = (SLNode *)malloc(sizeof(SLNode));
+        m->Data = x;
+
+        m->next = *head;
+        *head = m;
+        return 1;
+    }
+    else // 若不在第0个位置插入
+    {
+        p = *head;
+        j = 0;
+
+        while (p->next != NULL && j < i - 1)
+        // 最终让指针p指向第i-1个结点
         {
-            return list;
+            p = p->next;
+            j++;
         }
-        (*newNode).next = list;
-        list = newNode;
-        (*newNode).data = data;
-        return list;
+        if (j != i - 1)
+        {
+            printf("插入元素位置参数错！");
+            return 0;
+        }
+        q = (SLNode *)malloc(sizeof(SLNode));
+        q->Data = x;
+
+        q->next = p->next;
+        p->next = q;
+        return 1;
     }
-    return list;
 }
 
-// 插数据（指定位置）
-int insertList(LinkList list, int index, int data)
+// 删除新结点
+int ListDelete(SLNode **head, int i, DataType *x)
 {
-    LNode *tmp = list;
-    int j = 1;
-    if (list == NULL)
+    SLNode *p, *q, *m, *s;
+    int j;
+    if (i == 0) // 若在第0个位置删除
     {
-        return 0;
-    }
-    if (index < 2)
-    {
-        return 0;
-    }
-    while (tmp != NULL && j < index - 1)
-    {
+        m = *head;    // 让m指向第0个结点
+        *x = m->Data; // 把指针m所指的结点数据域值赋给x
 
-        tmp = (*tmp).next;
+        *head = m->next; // 删除
+        free(m);         // 释放m所指结点的内存空间
+        return 1;
+    }
+    else // 若不在第0个位置删除
+    {
+        p = *head;
+        j = 0;
+
+        while (p->next != NULL && p->next->next != NULL && j < i - 1)
+        // 循环结束时指针p指向第i-1个结点
+        {
+            p = p->next;
+            j++;
+        }
+
+        if (j != i - 1)
+        {
+            printf("删除元素位置错误！");
+            return 0;
+        }
+
+        s = p->next;
+        *x = s->Data;
+        p->next = p->next->next;
+        free(s);
+        return 1;
+    }
+}
+
+// 取数据元素
+int ListGet(SLNode *head, int i, DataType *x)
+{
+    SLNode *p;
+    int j;
+
+    p = head;
+    j = 0;
+
+    while (p->next != NULL && j < i)
+    {
+        p = p->next;
         j++;
     }
-
-    if (tmp == NULL)
+    if (j != i)
     {
+        printf("取数据元素位置错！");
         return 0;
     }
 
-    LNode *newNode = (LNode *)malloc(sizeof(LNode));
-    if (newNode == NULL)
-    {
-        return 0;
-    }
-    (*newNode).data = data;
-    (*newNode).next = (*tmp).next;
-    (*tmp).next = newNode;
+    *x = p->Data;
     return 1;
 }
 
-// 插数据（指定结点后）
-int insertNextNode(LNode *p, int data)
+// 撤销单链表
+void Destory(SLNode **head)
 {
-    if (p == NULL)
+    SLNode *p, *q;
+    p = *head;
+
+    while (p != NULL)
     {
-        return 0;
+        q = p;
+        p = p->next;
+        free(q);
     }
-    LNode *newNode = (LNode *)malloc(sizeof(LNode));
-    if (newNode == NULL)
-    {
-        return 0;
-    }
-    (*newNode).data = data;
-    (*newNode).next = (*p).next;
-    (*p).next = newNode;
-    return 1;
+    *head = NULL;
 }
-
-// 插数据（指定结点前（不包括第一个结点））【移动数据】
-int insertPriorNodeOne(LNode *p, int data)
+// 打印
+void printList(SLNode **head)
 {
-    if (p == NULL)
+    int x;
+    for (int i = 0; i < ListLength(*head); i++) // 显示当前数据元素
     {
-        return 0;
+        ListGet(*head, i, &x); // 取元素
+        printf("%d,", x);      // 显示
     }
-    LNode *newNode = (LNode *)malloc(sizeof(LNode));
-    if (newNode == NULL)
-    {
-        return 0;
-    }
-    (*newNode).next = (*p).next;
-    (*p).next = newNode;
-    (*newNode).data = (*p).data;
-    (*p).data = data;
-    return 1;
+    printf("\n--------------------------\n");
 }
-
-// 插数据（指定结点前（不包括第一个结点））【循环】
-int insertPriorNodeTwo(LinkList list, LNode *p, int data)
+// 删除值为x的节点
+void delete_x(LinkList &L, int x)
 {
-    //		if(list=NULL || p==NULL){
-    //		return 0;
-    //	}
-    LNode *tmp = list;
-
-    while (tmp != NULL && (*tmp).next != p)
+    SLNode *p;
+    if (L == NULL)
     {
-        tmp = (*tmp).next;
+        return;
     }
-
-    if (tmp == NULL)
+    else
     {
-        return 0;
-    }
-    LNode *newNode = (LNode *)malloc(sizeof(LNode));
-    if (newNode == NULL)
-    {
-        return 0;
-    }
-    (*newNode).next = p;
-    (*tmp).next = newNode;
-    (*newNode).data = data;
-    return 1;
-}
-
-// 删单链表的第一个位置
-LinkList deleteFirst(LinkList list, int index)
-{
-    if (index == 1)
-    {
-        LNode *tmp = list;
-        list = (*tmp).next;
-        //		*data = (*tmp).data;
-        free(tmp);
-        return list;
+        if (L->Data == x)
+        {
+            p = L;
+            L = L->next;
+            free(p);
+            delete_x(L, x);
+        }
+        else
+        {
+            delete_x(L->next, x);
+        }
     }
 }
-
-// 删单链表（除第一个位置）(指定位置)的数据
-int deleteList(LinkList list, int index)
-{
-    if (list == NULL)
-    {
-        return 0;
-    }
-
-    LNode *tmp = list;
-    int j = 1;
-    if (index < 2)
-    {
-        return 0;
-    }
-    while (tmp != NULL && j < index - 1)
-    {
-
-        tmp = (*tmp).next;
-
-        j++;
-    }
-    if (tmp == NULL)
-    {
-        return 0;
-    }
-    if ((*tmp).next == NULL)
-    {
-        return 0;
-    }
-    LNode *p = (*tmp).next;
-    //	int *data=(*p).data;
-    (*tmp).next = (*p).next;
-    free(p);
-    return 1;
-}
-
-// 删指定结点（不能删第一个和最后一个）【移动】
-int deleteNodeOne(LNode *p)
-{
-    if (p == NULL)
-    {
-        return 0;
-    }
-    LNode *q = (*p).next;
-    (*p).next = (*q).next;
-    (*p).data = (*q).data;
-    free(q);
-    return 1;
-}
-
-// 删指定结点【循环】
-int deleteNodeTwo(LinkList list, LNode *p)
-{
-    if (list == NULL)
-    {
-        return 0;
-    }
-
-    if (p == NULL)
-    {
-        return 0;
-    }
-
-    LNode *tmp = list;
-
-    while (tmp != NULL && (*tmp).next != p)
-    {
-        tmp = (*tmp).next;
-    }
-
-    if (tmp == NULL)
-    {
-        return 0;
-    }
-
-    (*tmp).next = (*p).next;
-    free(p);
-    return 1;
-}
-
-// 按位查找：返回指定位置的结点
-LNode *getElementByIndex(LinkList list, int index)
-{
-    LNode *tmp = list;
-    int j = 1;
-    if (index < 0)
-    {
-        return NULL;
-    }
-    while (tmp != NULL && j < index)
-    {
-        tmp = (*tmp).next;
-        j++;
-    }
-    printf("第二个位置：%d,", (*tmp).data);
-    return tmp;
-}
-
-// 按值查找：返回指定值的结点
-LNode *getElementByData(LinkList list, int data)
-{
-    LNode *tmp = list;
-
-    while (tmp != NULL && (*tmp).data != data)
-    {
-        tmp = (*tmp).next;
-    }
-    printf("值为3的位置：%d,", (*tmp).data);
-    return tmp;
-}
-
-// 求表长
-int getLength(LinkList list)
-{
-    if (list == NULL)
-    {
-        return 0;
-    }
-    int length = 1;
-    LNode *tmp = list;
-    while ((*tmp).next != NULL)
-    {
-        tmp = (*tmp).next;
-        length++;
-    }
-    return length;
-}
-
-// 新建一个链表：头插法--->相当于逆置链表
-LinkList constructLinkListOne(LinkList list)
-{
-    int i = 1;
-    for (; i < 5; i++)
-    {
-        list = insertFirst(list, 1, i + 1);
-        printf("%d,", (*list).next->data);
-    }
-    return list;
-}
-// 遍历
-void bianli(LinkList list)
-{
-    LNode *tmp = list;
-    while (tmp != NULL)
-    {
-        printf("%d,", (*tmp).data);
-        tmp = (*tmp).next;
-    }
-    printf("\n");
-}
-// 新建一个链表：尾插法
-void constructLinkListTwo(LinkList list)
-{
-    int i = 1, j = 1;
-    LNode *tmp = list;
-
-    for (; i < 5; i++)
-    {
-        insertList(list, j + 1, i + 1);
-        j++;
-    }
-    bianli(list);
-}
-
-// 清空
-void clearList(LinkList list)
-{
-    while (list != NULL)
-    {
-        LNode *tmp = list;
-        list = (*list).next;
-        free(list);
-    }
-}
-
 int main()
 {
-    int length = 0, i = 0;
-    LinkList list = initList(list);
+    SLNode *head;
+    int i, x;
+    ListInitiate(&head);     // 初始化
+    for (i = 0; i < 10; i++) // 插入10个数据元素
+    {
+        scanf("%d", &x);
+        ListInsert(&head, i, x);
+    }
+    printList(&head);
+    delete_x(head, 2);
+    printList(&head);
+    Destory(&head); /// 撤销单链表
+    return 0;
 }
